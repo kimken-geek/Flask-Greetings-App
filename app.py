67ef6617e1627c5079e62fb1f7ba20a3c8d6b10b
kimken-geek <kennethkimken@gmail.com>
2022-05-16 01:07:51 +0300

    Flask App

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key ='qwtybkTT6G'
+
+@app.route("/")
+def index():
+    flash("What is your name?")
+    return render_template('index.html')
+
+@app.route("/greet", methods=['POST', 'GET'])
+def greeter():
+	flash("Hi " + str(request.form['name_input']) + ", great to see you!")
+	return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run()
